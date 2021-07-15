590b7a44ce84f94538fc333f9aef763b7a03f258
Tabataalves2215 <tabataalves2215@gmail.com>
2021-07-15 18:10:03 -0300

    Escape

---
+++ js/main.js
@@ -0,0 +1,103 @@
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function validaIdade(idade){
+    var validar = true;
+    if(idade >= 18){
+validar = true
+    }else{
+        validar = false
+    }
+    
+}
+var idade = prompt("Qual a sua idade");
+validaIdade(idade);
+console.log(validar);
+
+//alert(soma(5, 10));
+
+
+
+
+
+
+
+/*var d = new Date();
+alert(d.getMonth());
+alert(d.getDate());
+alert(d.getMinutes());
+alert(d.getHours());/*
+
+
+/*var count;
+for(count=0; count <= 1; count++){
+    alert(count);
+};/*
+
+
+
+
+
+/*var count = 8;
+while (count <= 6){
+    console.log(count);
+    alert (count);
+    count = count++;
+};/*
+
+
+
+
+
+
+/*var idade = prompt("Qual sua idade?");
+if(idade >= 18){
+    alert("maior de idade");
+}else{ 
+    alert("menor de idade");
+
+} /*
+
+
+
+
+
+/*var frutas = [{nome: "morango", cor: "vermelha"} , {nome: "uva", cor: "roxa"}]
+console.log (frutas);
+alert (frutas[1].nome);/*
+
+
+
+
+/*var fruta = {nome: "morango", cor: "vermelha"}
+console.log (fruta.nome);
+alert (fruta.cor);/*
+
+
+//var lista = ["maçã", "uva", "morango"];
+//lista.pop ();
+//lista.push("laranja");
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+//alert ([1]);
+
+
+
+
+
+
+
+//var nome = "Tábata Alves";
+//var n1 = 2;
+//var n2 = 5;
+//var frase = "Coreia é o Melhor País do Mundo!!";
+
+
+//alert (nome + "tem" + $idade + " anos");
+//alert (idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toUpperCase());
+//alert(frase.replace("Coreia" , "Brasil"));
